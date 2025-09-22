3400a1d320bb9d36ce4b23d3e42f09f6e60735a6
javibash <nieto.jb@gmail.com>
2025-09-21 21:39:25 -0600

    Actualizacion

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ load_dotenv()
 
 # Quick-start development settings
 SECRET_KEY = 'django-insecure-41prgf+=eq2q))u_!feo2!c4+xdg27_8k_1n%c((e--&w#60u9'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["mapadjango.onrender.com", "localhost"]
 
 
